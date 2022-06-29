4003c9a1df121d69c2113d6e133ef29058a6c08b
kylebaugh <kylebaugh1@gmail.com>
2022-06-29 12:58:25 -0600

    testing 2

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require ('express')
+const express = require('express')
 const path = require('path')
 
 const app = express()
@@ -14,3 +14,27 @@ const port =  process.env.PORT || 4005
 app.listen(port, () => {
     console.log(`server running on ${port}`)
 })
+
+
+// const express = require('express')
+// const path = require('path')
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'))
+// })
+
+// app.get('/css', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../styles.css'))
+// })
+
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.js'))
+// })
+
+// const port = process.env.PORT || 4005
+
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`)
+// })
